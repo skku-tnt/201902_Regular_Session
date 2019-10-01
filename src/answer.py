f5e8136aa98568b15e12ad1db8b8592bf8799409
YONGSUN YOON <respect5716@gmail.com>
2019-10-01 19:06:42 +0900

    np.std ddof 수정

--- src/answer.py
+++ src/answer.py
@@ -57,7 +57,7 @@ class featureNormalize:
     }
     def test(X):
         mu = np.mean(X, axis=0)
-        sigma = np.std(X, axis=0)
+        sigma = np.std(X, axis=0, ddof=1)
         X_norm = (X - mu) / sigma
         return X_norm, mu, sigma
 
